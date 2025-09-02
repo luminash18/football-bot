d755b03648ea8fddd2a4d680310e198397882348
Luminash Patel <110195433+luminash18@users.noreply.github.com>
2025-09-02 11:54:08 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -66,7 +66,6 @@ def get_twitter_handle_from_source(source_url):
     """Map RSS feed URL to Twitter handle"""
     handle_map = {
         "https://www.goal.com/feeds/en/news": "@goal",
-        "https://www.skysports.com/rss/12040": "@SkySports",
         "https://feeds.bbci.co.uk/sport/football/rss.xml": "@BBCSport",
         "https://www.theguardian.com/football/rss": "@GuardianSport",
     }
